0774ce8b40057b7ceab1caf241ae2a773b815cc0
EduardoDev <iduuh4@gmail.com>
2023-11-22 22:01:39 -0300

    resolvendo bugs no display mobile

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const toggleButton = document.querySelector(".button-mobile");
 const sidebar = document.querySelector(".sidebar");
 const toggleButtonClose = document.querySelector(".button-mobile-close");
+const overlay = document.querySelector(".main");
 
 toggleButton.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   toggleButton.style.display = "none";
   toggleButtonClose.style.display = "flex";
   toggleButtonClose.style.position = "fixed";
+  main.classList.add(".overlay");
 });
 
 toggleButtonClose.addEventListener("click", () => {
